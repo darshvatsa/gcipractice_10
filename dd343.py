aaa37b3ef6eba3e28d8f782a78d180c01a4697b6
darshvatsa <44691030+darshvatsa@users.noreply.github.com>
2018-11-02 09:18:18 +0530

    Uploaded!

---
+++ dd343.py
@@ -0,0 +1,18 @@
+print ("Wecome to GCI 2018")
+
+for a in range(1,11):
+    print ("GCI is Great")
+
+
+name=input("Hello! Enteryour name: ")
+
+b=len(name)-1
+name1=""
+while b>=0:
+    name1=name1+name[b]
+    b=b-1
+    
+print ("Hello",name ," please to meet you! Did you know that your name backwards is",name1,"?")
+
+
+
